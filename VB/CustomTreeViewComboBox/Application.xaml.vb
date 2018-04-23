cad8cb1fa22a2be8b52249ecf8a9064e8762598b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomTreeViewComboBox/Application.xaml.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Configuration
+Imports System.Data
+Imports System.Linq
+Imports System.Windows
+Imports DevExpress.Xpf.Core
+
+Namespace CustomTreeViewComboBox
+	''' <summary>
+	''' Interaction logic for App.xaml
+	''' </summary>
+	Partial Public Class App
+		Inherits Application
+		Protected Overrides Sub OnStartup(ByVal e As StartupEventArgs)
+			MyBase.OnStartup(e)
+			ThemeManager.ApplicationThemeName = "Office2007Blue"
+
+		End Sub
+	End Class
+End Namespace
---
+++ VB/CustomTreeViewComboBox/MainWindow.xaml.vb
@@ -0,0 +1,124 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Grid
+Imports System.Collections
+Imports System.Collections.ObjectModel
+
+Namespace CustomTreeViewComboBox
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			AddHandler Me.Loaded, AddressOf MainWindow_Loaded
+		End Sub
+
+		Private Sub MainWindow_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Me.DataContext = Me
+			lookUpEdit.ItemsSource = Stuff.GetStuff()
+		End Sub
+
+
+		Private Sub lookUpEdit_PopupClosing_1(ByVal sender As Object, ByVal e As DevExpress.Xpf.Editors.ClosePopupEventArgs)
+
+		End Sub
+
+		Private Sub lookUpEdit_PopupClosed_1(ByVal sender As Object, ByVal e As DevExpress.Xpf.Editors.ClosePopupEventArgs)
+
+		End Sub
+
+	End Class
+
+	Public Class Employee
+		Private privateID As Integer
+		Public Property ID() As Integer
+			Get
+				Return privateID
+			End Get
+			Set(ByVal value As Integer)
+				privateID = value
+			End Set
+		End Property
+		Private privateParentID As Integer
+		Public Property ParentID() As Integer
+			Get
+				Return privateParentID
+			End Get
+			Set(ByVal value As Integer)
+				privateParentID = value
+			End Set
+		End Property
+		Private privateName As String
+		Public Property Name() As String
+			Get
+				Return privateName
+			End Get
+			Set(ByVal value As String)
+				privateName = value
+			End Set
+		End Property
+		Private privatePosition As String
+		Public Property Position() As String
+			Get
+				Return privatePosition
+			End Get
+			Set(ByVal value As String)
+				privatePosition = value
+			End Set
+		End Property
+		Private privateDepartment As String
+		Public Property Department() As String
+			Get
+				Return privateDepartment
+			End Get
+			Set(ByVal value As String)
+				privateDepartment = value
+			End Set
+		End Property
+	End Class
+
+	Public NotInheritable Class Stuff
+		Private Sub New()
+		End Sub
+		Public Shared Function GetStuff() As List(Of Employee)
+			Dim stuff As New List(Of Employee)()
+			stuff.Add(New Employee() With {.ID = 1, .ParentID = 0, .Name = "Gregory S. Price", .Department = "", .Position = "President"})
+			stuff.Add(New Employee() With {.ID = 2, .ParentID = 1, .Name = "Irma R. Marshall", .Department = "Marketing", .Position = "Vice President"})
+			stuff.Add(New Employee() With {.ID = 3, .ParentID = 1, .Name = "John C. Powell", .Department = "Operations", .Position = "Vice President"})
+			stuff.Add(New Employee() With {.ID = 4, .ParentID = 1, .Name = "Christian P. Laclair", .Department = "Production", .Position = "Vice President"})
+			stuff.Add(New Employee() With {.ID = 5, .ParentID = 1, .Name = "Karen J. Kelly", .Department = "Finance", .Position = "Vice President"})
+
+			stuff.Add(New Employee() With {.ID = 6, .ParentID = 2, .Name = "Brian C. Cowling", .Department = "Marketing", .Position = "Manager"})
+			stuff.Add(New Employee() With {.ID = 7, .ParentID = 2, .Name = "Thomas C. Dawson", .Department = "Marketing", .Position = "Manager"})
+			stuff.Add(New Employee() With {.ID = 8, .ParentID = 2, .Name = "Angel M. Wilson", .Department = "Marketing", .Position = "Manager"})
+			stuff.Add(New Employee() With {.ID = 9, .ParentID = 2, .Name = "Bryan R. Henderson", .Department = "Marketing", .Position = "Manager"})
+
+			stuff.Add(New Employee() With {.ID = 10, .ParentID = 3, .Name = "Harold S. Brandes", .Department = "Operations", .Position = "Manager"})
+			stuff.Add(New Employee() With {.ID = 11, .ParentID = 3, .Name = "Michael S. Blevins", .Department = "Operations", .Position = "Manager"})
+			stuff.Add(New Employee() With {.ID = 12, .ParentID = 3, .Name = "Jan K. Sisk", .Department = "Operations", .Position = "Manager"})
+			stuff.Add(New Employee() With {.ID = 13, .ParentID = 3, .Name = "Sidney L. Holder", .Department = "Operations", .Position = "Manager"})
+
+			stuff.Add(New Employee() With {.ID = 14, .ParentID = 4, .Name = "James L. Kelsey", .Department = "Production", .Position = "Manager"})
+			stuff.Add(New Employee() With {.ID = 15, .ParentID = 4, .Name = "Howard M. Carpenter", .Department = "Production", .Position = "Manager"})
+			stuff.Add(New Employee() With {.ID = 16, .ParentID = 4, .Name = "Jennifer T. Tapia", .Department = "Production", .Position = "Manager"})
+
+			stuff.Add(New Employee() With {.ID = 17, .ParentID = 5, .Name = "Judith P. Underhill", .Department = "Finance", .Position = "Manager"})
+			stuff.Add(New Employee() With {.ID = 18, .ParentID = 5, .Name = "Russell E. Belton", .Department = "Finance", .Position = "Manager"})
+			Return stuff
+		End Function
+	End Class
+End Namespace
